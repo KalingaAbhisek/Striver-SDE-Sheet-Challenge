2c1683017fc8944bfd61dc88b406a36c8900fc61
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-06-05 00:46:01 +0530

    Update Set Matrix Zeros.java

--- DAY 1 04-06-2022/Set Matrix Zeros.java
+++ DAY 1 04-06-2022/Set Matrix Zeros.java
@@ -1,1 +1,33 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        // Write your code here..
+        boolean firstColZero = false, firstRowZero = false;
+        for(int i = 0;i < matrix.length;i++)
+            if(matrix[i][0] == 0)
+                firstColZero = true;
+        for(int j = 0;j < matrix[0].length;j++)
+            if(matrix[0][j] == 0)
+                firstRowZero = true;
+        for(int i = 1;i < matrix.length;i++)
+            for(int j = 1;j < matrix[0].length;j++)
+                if(matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        for(int i = 1;i < matrix.length;i++)
+            if(matrix[i][0] == 0)
+                for(int j = 0;j < matrix[0].length;j++)
+                    matrix[i][j] = 0;
+        for(int j = 1;j < matrix[0].length;j++)
+            if(matrix[0][j] == 0)
+                for(int i = 0;i < matrix.length;i++)
+                    matrix[i][j] = 0;
+        if(firstColZero)
+            for(int i = 0;i < matrix.length;i++)
+                matrix[i][0] = 0;
+        if(firstRowZero)
+            for(int j = 0;j < matrix[0].length;j++)
+                matrix[0][j] = 0;
+    }
 
+}
