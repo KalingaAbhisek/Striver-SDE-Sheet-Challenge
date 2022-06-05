9fdc97b8aef46b893b78b00a0e7fe62b7f53cf7f
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-06-05 15:01:38 +0530

    Create Maximum Subarray Sum.java

---
+++ DAY 2 05-06-2022/Maximum Subarray Sum.java
@@ -0,0 +1,22 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+        long maxsum=0;
+        long currsum=0;
+        for(int i=0;i<n;i++){
+            currsum+=arr[i];
+            if(currsum>maxsum){
+                maxsum=currsum;
+            }
+            if(currsum<0){
+                currsum=0;
+            }
+        }
+        return maxsum;
+	}
+
+}
