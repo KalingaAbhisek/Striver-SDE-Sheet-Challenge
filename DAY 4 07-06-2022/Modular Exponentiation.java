184050ced73cd269a2ad3011a033ce3c4945f69a
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-06-07 22:31:22 +0530

    Create Modular Exponentiation.java

---
+++ DAY 4 07-06-2022/Modular Exponentiation.java
@@ -0,0 +1,17 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int modularExponentiation(int x, int n, int m) {
+        // Write your code here.      
+        long ans=1;
+        long temp=x;
+        while(n>0){
+            if(n%2!=0){
+                ans=((ans%m)*temp%m)%m;
+            }
+            temp=((temp%m)*(temp%m))%m;
+            n=n>>1;
+        }
+        return (int) ans%m;
+    }
+}
