204d66208b98261ef31ffa037d53b62dd973cfd4
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-06-23 09:12:42 +0530

    Create LCA of Two Nodes In A BST.java

---
+++ DAY 20 23-06-2022/LCA of Two Nodes In A BST.java
@@ -0,0 +1,43 @@
+/*
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T> 
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data) 
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+*/
+import java.util.*;
+public class Solution 
+{
+
+	public static TreeNode<Integer> LCAinaBST(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) 
+    {		
+        // Write your code here
+        if(root==null || root==p || root==q){
+            return root;
+        }
+        TreeNode<Integer> left=LCAinaBST(root.left,p,q);
+        TreeNode<Integer> right=LCAinaBST(root.right,p,q);
+        if(left==null){
+            return right;
+        }
+        else if(right==null){
+            return left;
+        }
+        else{
+            return root;
+        }
+	}
+}
