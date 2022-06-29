a88f9a67e837465d92f40d186d51e4af1cb03a3a
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2022-06-30 00:03:57 +0530

    Create Check Identical Trees.java

---
+++ DAY 26 29-06-2022/Check Identical Trees.java
@@ -0,0 +1,27 @@
+/*******************************************************
+
+    Following is the BinaryTreeNode class structure:
+
+    class BinaryTreeNode<T> {
+        T data;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+        
+        public BinaryTreeNode(T data) {
+            this.data = data;
+        }
+    }
+
+*******************************************************/
+
+public class Solution {
+    
+    public static boolean identicalTrees(BinaryTreeNode<Integer> p, BinaryTreeNode<Integer> q) {
+        // Write you code here.  
+        if(p==null || q==null){
+            return p==q;
+        }
+        return (p.data.equals(q.data)) && identicalTrees(p.left,q.left) && identicalTrees(p.right,q.right);
+    }
+
+}
